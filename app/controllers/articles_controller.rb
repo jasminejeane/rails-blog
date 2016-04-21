2f4c464cbd70b79c474c225205ac7da227ead173
Jasmine Martin <jasminejeane@gmail.com>
2016-04-20 19:48:46 -0700

    new route and form created

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,7 @@
+class ArticlesController < ApplicationController
+	# this is a plain action rails. nothing defined within
+	# it. Rails expects a view to be associated with it
+	def new
+	end
+
+end
